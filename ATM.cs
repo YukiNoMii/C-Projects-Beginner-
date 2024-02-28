131e7c68eff1eaf3ff61e5e15d5d8dc4a1a462e6
YukiNoMii <161603254+YukiNoMii@users.noreply.github.com>
2024-02-28 21:19:32 +0800

    Simple ATM
    Created at 2/26/24 - 2/28/24

---
+++ ATM.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace atm;
+
+public class ATM 
+{ 	
+      public string input;
+      private string name;
+	  public string Name
+	  {
+          get { return name; }
+		  set{
+              name = value;
+			  
+			  if(int.TryParse(name, out int nameNum))
+			  {
+                  Console.WriteLine("You can't put numbers on your name");
+				  Name = Console.ReadLine();				  
+              }
+          }
+      }
+	  private string password;
+	  public string Password
+	  {
+          get { return password; }
+		  
+	 	 set
+	 	 {
+              password = value;
+			  
+			  int passwordLength = password.Length;
+		      bool getLength = false;   
+			  while(!getLength)
+			  {
+                  if(int.TryParse(password, out int passwordNum) && passwordLength > 7 || passwordLength > 7)
+				  {
+                    value = passwordNum.ToString();
+					Console.WriteLine("Your Password has been set");
+					getLength = true;
+					
+                  }
+				  if(int.TryParse(password, out passwordNum) && passwordLength < 8 || passwordLength < 8)
+				  {
+                     Console.WriteLine("Your password needs to have 8 characters");
+					 Password = Console.ReadLine();
+                     break;
+	              }
+              }			  
+		  }			  
+      }
+	  private int balance;
+	  public int Balance
+	  {
+          get {return balance;}
+		  
+		  set{
+              balance = value;
+			  
+			  if(value > 1000)
+			  {
+                  Console.WriteLine("You can't put more than 1000 in your balance,");
+              }
+          }
+      }
+	  
+	  private int loanBalance;
+	  public int LoanBalance
+	  {
+          get{return loanBalance;}
+		  
+		  set{
+              loanBalance = value;
+			  
+			  if(value > 1000 || loanBalance > 1000)
+			  {
+                  Console.WriteLine("You can't get more than 1000 in loan,");
+              }              
+			  else
+			  {
+                  loanBalance = value;
+              }
+          }
+      }
+	  
+	  public void BalanceNLoan()
+	  {
+         input = Console.ReadLine();
+         while(input != "e")
+		 {	          
+           if(input == "1")
+		   {
+               GetBalance();
+           }
+		   else if(input == "2")
+		   {
+               GetLoan();
+           }
+		 }
+      }
+	  private void GetBalance()
+	  {	
+          if(input == "1")
+		  {
+              Console.WriteLine("Your Account's Balance:");
+			  Console.WriteLine(LoanBalance);
+			  input = Console.ReadLine();
+          }		
+      }
+	  
+	  private void GetLoan()
+	  {	          
+          Console.WriteLine("How many dollars do you wanna loan?");
+		  string user = Console.ReadLine();
+		  if(int.TryParse(user, out int getLoan))
+		  {
+              if(getLoan > 1000)
+			  {
+                  Console.WriteLine("You can't loan more than 1000.");
+              }
+			  else
+			  {
+                  Console.WriteLine($"{getLoan} have been added to your balance. ");
+				   LoanBalance = getLoan;
+				  Console.WriteLine(LoanBalance);
+				  Console.WriteLine("Input Number 1 to see your balance or enter 'e' to exit");
+				  input = Console.ReadLine();
+				  if(input == "1")
+				  {
+                      GetBalance();
+                  }
+                  else
+				  {
+                      Console.WriteLine("ATM has been closed.");
+                  }				
+              }
+          }
+      }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using atm;
+
+namespace program;
+
+public static class Program 
+{         
+    public static void Main()
+    {
+        ATM atm = new ATM();
+		string user = "";
+		bool isVerified = false;
+        Console.WriteLine("Hello, Would you Like to Register or Login to your atm?");	
+		
+		while(user != "e")
+		{ 
+            user = Console.ReadLine();
+            if(user == "register" || user == "Register")
+			{
+                Console.WriteLine("What Would you Like you name to be set as?");
+				atm.Name = Console.ReadLine();
+				Console.WriteLine($"Registered as {atm.Name}");
+				Console.WriteLine("How would you like to be your password set as?");
+				atm.Password = Console.ReadLine();								
+                if(atm.Password == atm.Password)
+				{ 					    
+					Console.WriteLine("Input login to login your account or 'e' to exit");
+					user = Console.ReadLine();
+			    }
+            }
+			if(user == "login" || user ==  "Login")
+			{
+                Console.WriteLine("Input your name's account");
+				user = Console.ReadLine();
+				
+				if(user != atm.Name)
+				{
+                    Console.WriteLine($"{user} incorrect name, Try again");
+					user = Console.ReadLine();							
+                }
+				else
+                Console.WriteLine($"{user} has been verified. Input password");   		
+                						
+	   		 if(Console.ReadLine() != atm.Password)
+				{
+                    Console.WriteLine($"{user} incorrect password, Try again");
+				    user = Console.ReadLine();				  
+                }			    
+				else
+				{
+                    Console.Clear();
+                    Console.WriteLine($"{user} account successfully verified.");               								                  
+					Console.WriteLine("===============================");
+					Console.WriteLine($"|   {atm.Name} Account Info  q|");
+					Console.WriteLine("| (1)Balance: 0    (2)Loan    |");
+					Console.WriteLine("===============================");				
+					atm.BalanceNLoan();					
+                }								
+            }
+			if(user == "e")
+			{
+                Console.WriteLine("You Have the ATM");
+				break;
+            }
+            break;
+        }
+	}
+}
